caa4c0edd271ad61e47464a079ae777ddabcd422
Arman Sani <armanisani@gmail.com>
2016-05-10 14:43:20 -0700

    mobile model finished added pages to the eccommerce model

--- api/models/Eccommerce.js
+++ api/models/Eccommerce.js
@@ -6,6 +6,7 @@ var eccommerceSchema = mongoose.Schema({
   title: {type: String, required: true},
   product: {type: String, required: true},
   color: {type: String, required: true},
+  pages: {type: Number, required: true}
   company: String,
   onlinePayments: {type: String, required: true},
   description: {type: String, required: true},
--- api/models/Mobile.js
+++ api/models/Mobile.js
@@ -2,4 +2,19 @@ var
   mongoose = require('mongoose')
 
 
-var mobileSchema = 
+var mobileSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  phoneType: {type: String, required: true},
+  tabs: {type: Number, required: true},
+  type: {type: String, required: true},
+  color: {type: String, required: true},
+  description: {type: String, required: true},
+  additionalInfo: String
+})
+
+
+var Mobile = mongoose.model('Mobile', mobileSchema)
+
+
+
+module.exports = Mobile
