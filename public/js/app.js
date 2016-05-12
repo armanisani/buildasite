de46119f90b4f7a61f08d82dbf05f7e2a5d71d27
Arman Sani <armanisani@gmail.com>
2016-05-12 15:59:31 -0700

    got the front t page started and layout is all there now need to write content

--- public/js/app.js
+++ public/js/app.js
@@ -6,42 +6,42 @@ angular.module('buildasite', ['buildasite.controllers', 'buildasite.services'])
   $stateProvider
   .state('home', {
     url: '/',
-    templateUrl: '../.././public/index.html',
+    templateUrl: '../public/index.html',
     controller: 'MainCtrl as main'
   })
   .state('profile', {
     url: '/profile/:id',
-    templateUrl: '../.././public/profile.html',
+    templateUrl: '../public/profile.html',
     controller: 'ProfileCtrl as profile'
   })
   .state('buildasite', {
     url: '/build',
-    templateUrl: '../.././public/build.html',
+    templateUrl: '../public/build.html',
     controller: 'BuildCtrl as bc'
   })
   .state('websites', {
     url: '/websites',
-    templateUrl: '../.././public/websites.html',
+    templateUrl: '../public/websites.html',
     controller: 'WebsiteCtrl as website'
   })
   .state('ecommerceDetail', {
     url: '/websites/ecommerce/:id',
-    templateUrl: '../.././public/ecommerceDetail.html',
+    templateUrl: '../public/ecommerceDetail.html',
     controller: 'EcommerceDetailCtrl as ec'
   })
   .state('mobileDetail', {
     url: '/websites/mobile/:id',
-    templateUrl: '../.././public/mobileDetail.html',
+    templateUrl: '../public/mobileDetail.html',
     controller: 'MobileDetailCtrl as mc'
   })
   .state('singleDetail', {
     url: '/websites/single/:id',
-    templateUrl: '../.././public/singleDetail.html',
+    templateUrl: '../public/singleDetail.html',
     controller: 'SingleDetailCtrl as sc'
   })
   .state('socialDetail', {
     url: '/websites/social/:id',
-    templateUrl: '../.././public/socialDetail.html',
+    templateUrl: '../public/socialDetail.html',
     controller: 'SocialDetailCtrl as sc'
   })
 })
